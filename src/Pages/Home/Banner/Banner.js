cf19a1817b4bb563d2bcf6b0c01d9f79123d2577
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-04-18 05:08:01 +0600

    slider updated with text big title and with responsive for mobile and big screen

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-import banner1 from '../../../Images/Banner/banner-1.jpg'
+import './Banner.css';
+import banner1 from '../../../Images/Banner/banner-1.jpeg'
 import banner2 from '../../../Images/Banner/banner-2.jpg'
 import banner3 from '../../../Images/Banner/banner-3.jpg'
 
@@ -14,39 +15,39 @@ const Banner = () => {
         <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
           <img
-            className="d-block w-100 h-50"
+            className="d-block w-100"
             src= {banner1}
-            alt="First slide"
+            alt="Explore Beautiful Bangladesh"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Carousel.Caption className='d-flex flex-column h-100 align-items-center justify-content-center'>
+            <h1 style={{color:''}}>Explore Beautiful Bangladesh</h1>
+            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100 h-50"
+            className="d-block w-100"
             src={banner2}
             alt="Second slide"
           />
   
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Carousel.Caption className='d-flex flex-column h-100 align-items-center justify-content-center'>
+            <h1>TRAVEL WITH TAUHID RANA</h1>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100 h-50"
+            className="d-block w-100"
             src={banner3}
             alt="Third slide"
           />
   
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
+          <Carousel.Caption className='d-flex flex-column h-100 align-items-center justify-content-center'>
+            <h1 style={{color:'seagreen'}}>EXPERIENCE THE BEAUTY OF NATURE</h1>
+            {/* <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            </p> */}
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,26 +4,21 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 const Header = () => {
     return (
         <>
-            <Navbar sticky="top" collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Travel Guider</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#packages">Packages</Nav.Link>
+                            <Nav.Link href="#blogs">Blogs</Nav.Link>
+                            
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link href="#deets">About</Nav.Link>
                             <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
+                               Sign In
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
